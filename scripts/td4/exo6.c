cc530bc0afad6b52f39919852af13873eda61b30
Quickinline <adelbouhraoua23@gmail.com>
2022-01-24 16:10:11 +0100

    added exo 6

--- scripts/td4/exo6.c
+++ scripts/td4/exo6.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(int argc, char const *argv[])
 {
     MPI_Init(NULL, NULL);
-    
+
     int world_size, rank, name_len;
     char processor_name[30];
 
@@ -18,43 +17,62 @@ int main(int argc, char const *argv[])
     int A[5][10];
     int line[10];
 
-
-    if(rank == 0){
+    if (rank == 0)
+    {
         // if main process
-        int steps = 200;
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 10; j++)
             {
-                A[i][j] = i+j;
-                B[i][j] = A[i][j] - 2;
+                A[i][j] = i + j;
+                B[j][i] = A[i][j] - 2;
             }
-            
         }
-        
-        MPI_Bcast(&B, 10 * 5, MPI_INT, 0, MPI_COMM_WORLD);
-
-
-    }else{
-        // if other processes
-
-        MPI_Bcast(&B, 10 * 5, MPI_INT, 0, MPI_COMM_WORLD);
-        
-        printf("\n received B ");        
-        
     }
 
-    MPI_Scatter(&A, 10, MPI_INT, &line, 10, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&B, 10 * 5, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if(rank != 0){
-        printf("\nProcessus %d a reçu %d donnée [ ", rank, 10);
+    MPI_Scatter(&A, 5, MPI_INT, &line, 5, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 1)
+    {
         for (int i = 0; i < 10; i++)
         {
-            printf("%d, ",line[i]);
+            for (int j = 0; j < 5; j++)
+            {
+                printf("%d,", B[i][j]);
+            }
+            printf("\n ");
+        }
+    }
+
+    // affichage des données de chaque thread
+    printf("\nProcessus %d a reçu %d donnée [ ", rank, 10);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d, ", line[i]);
+    }
+    printf("]");
+
+    int result[5];
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            result[j] += line[i] * B[i][j];
+        }
+    }
+
+    if (rank == 1)
+    {
+        printf("\nProcessus %d a calculé %d donnée [ ", rank, 10);
+        for (int i = 0; i < 10; i++)
+        {
+            printf("%d, ", result[i]);
         }
         printf("]");
     }
-
 
     MPI_Finalize();
     return 0;
